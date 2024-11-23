a4769490472aeef5fcc586ffe1e2c3bd21f42367
DodecaDonk <98620922+DodecaDonk@users.noreply.github.com>
2024-11-23 15:08:40 -0800

    app.svelte
    Created app.svelte page

---
+++ lign167fp/src/components/app.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let name = "World";
+</script>
+
+<main>
+    <h1>Hello {name}!</h1>
+    <p>Visit <a href="https://svelte.dev/tutorial"> Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <input placeholder="What do you need to do?" value="" />
+</main>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;700&display=swap');
+    main {
+        text-align: center;
+        font-family: 'Nunito', sans-serif;
+    }
+    h1 {
+        font-size: 72px;
+        font-weight: 300;
+    }
+</style>
